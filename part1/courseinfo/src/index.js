2c92703ac587978062bc86e59c04d7466f0cab6b
maattaa <60356709+maattaa@users.noreply.github.com>
2020-09-03 18:43:18 +0300

    mv part1/* part1/courseinfo/*

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -43,9 +43,12 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <div>
-      <Part part={props.part1} excercises={props.exercises1} />
-      <Part part={props.part2} excercises={props.exercises2} />
-      <Part part={props.part3} excercises={props.exercises3} />
+      <Part part={props.part1}
+      excercises={props.exercises1} />
+      <Part part={props.part2}
+      excercises={props.exercises2} />
+      <Part part={props.part3}
+      excercises={props.exercises3} />
     </div>
   )
 }
