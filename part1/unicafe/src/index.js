a60fa6ca258014d7b0442e5a0cb39c68a69bf396
maattaa <60356709+maattaa@users.noreply.github.com>
2020-09-05 19:32:34 +0300

    Part 1.8 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,16 +2,24 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const SetFeedback = ({handleClick, text}) => (<button onClick={handleClick}>{text}</button>)
-const GetFeedback = ({feedback, number}) => (<p>{feedback} {number}</p>)
 
 //total required a lot
 const total = (a, b, c) => (a + b + c)
 
-const TotalFeedback = ({good, neutral, bad}) => (<p>all {total(good, neutral, bad)} </p>)
-const Positive = ({good, neutral, bad}) => (<p>positive {(good / total(good, neutral, bad))*100} %</p>)
-
 //on Average good=1, neutral =0, bad = -1, so (good - bad) should do
-const Average = ({good, neutral, bad}) => (<p>average {(good - bad) / total(good, neutral, bad)}</p>)
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total(good, neutral, bad)}</p>
+      <p>average {(good - bad) / total(good, neutral, bad)}</p> 
+      <p>positive {(good / total(good, neutral, bad))*100} %</p>
+    </>
+  )
+}
 
 const App = () => {
   // save clicks of each button to own state
@@ -25,13 +33,8 @@ const App = () => {
       <SetFeedback handleClick={() => setGood(good +1)} text = "good" />
       <SetFeedback handleClick={() => setNeutral(neutral + 1)} text = "neutral" />
       <SetFeedback handleClick={() => setBad(bad + 1)} text = "bad" />
-      <h1>statistics</h1>
-      <GetFeedback feedback="good" number={good} />
-      <GetFeedback feedback="neutral" number={neutral} />
-      <GetFeedback feedback="bad" number={bad} />
-      <TotalFeedback good={good} neutral={neutral} bad={bad} />
-      <Average good={good} neutral={neutral} bad={bad} />
-      <Positive good={good} neutral={neutral} bad={bad} />
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
