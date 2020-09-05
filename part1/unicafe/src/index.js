dc4821247f59b08cac44a9a1ec7a7d8d963a19aa
maattaa <60356709+maattaa@users.noreply.github.com>
2020-09-05 15:37:05 +0300

    1.7 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const SetFeedback = ({handleClick, text}) => (<button onClick={handleClick}>{text}</button>)
+const GetFeedback = ({feedback, number}) => (<p>{feedback} {number}</p>)
 
-const SetFeedback = ({handleClick, text}) => <button onClick={handleClick}>{text}</button> 
+//total required a lot
+const total = (a, b, c) => (a + b + c)
 
-const GetFeedback = ({feedback, number}) => <p>{feedback} {number}</p> 
+const TotalFeedback = ({good, neutral, bad}) => (<p>all {total(good, neutral, bad)} </p>)
+const Positive = ({good, neutral, bad}) => (<p>positive {(good / total(good, neutral, bad))*100} %</p>)
 
+//on Average good=1, neutral =0, bad = -1, so (good - bad) should do
+const Average = ({good, neutral, bad}) => (<p>average {(good - bad) / total(good, neutral, bad)}</p>)
 
 const App = () => {
   // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
@@ -23,9 +29,12 @@ const App = () => {
       <GetFeedback feedback="good" number={good} />
       <GetFeedback feedback="neutral" number={neutral} />
       <GetFeedback feedback="bad" number={bad} />
+      <TotalFeedback good={good} neutral={neutral} bad={bad} />
+      <Average good={good} neutral={neutral} bad={bad} />
+      <Positive good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
