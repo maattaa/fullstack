6d13c7dcd6421dfe85735e072e77c772095f17dd
Antti Määttänen <antti.maattanen@ecraft.com>
2020-10-03 18:58:18 +0300

    5.14 done

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,44 +1,53 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'Antti Määttänen',
-    likes: 44124,
-    url: 'www.testurl.com',
-    user: {
-      username: 'antti',
-      name: 'testikayttaja',
-      id: '5f757c510999809368b19cc4'
-    }
-  }
-
-  const user = {
+const blog = {
+  title: 'Component testing is done with react-testing-library',
+  author: 'Antti Määttänen',
+  likes: 44124,
+  url: 'www.testurl.com',
+  user: {
     username: 'antti',
     name: 'testikayttaja',
     id: '5f757c510999809368b19cc4'
   }
+}
+
+const user = {
+  username: 'antti',
+  name: 'testikayttaja',
+  id: '5f757c510999809368b19cc4'
+}
+
+test('renders title and author, not url or likes', () => {
+
 
   const component = render(
     <Blog blog={blog} user={user} />
   )
 
-  component.debug()
-  expect(component.container).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
-  expect(component.container).toHaveTextContent(
-    'Antti Määttänen'
-  )
-  const url = component.queryByText('www.testurl.com')
+  expect(component.container).toHaveTextContent(blog.title)
+  expect(component.container).toHaveTextContent(blog.author)
+  const url = component.queryByText(blog.url)
   expect(url).toBeNull()
 
-  const likes = component.queryByText('44124')
+  const likes = component.queryByText(blog.likes.toString())
   expect(likes).toBeNull()
 
+})
 
+test('clicks button, renders url and likes', () => {
+
+  const component = render(
+    <Blog blog={blog} user={user} />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(blog.url)
+  expect(component.container).toHaveTextContent(blog.likes)
 
 })
