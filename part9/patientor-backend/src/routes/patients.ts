71dadaa9e8aa79dbc500815caecd8853d8c146cc
Antti Määttänen <antti.maattanen@ecraft.com>
2020-11-22 22:58:01 +0200

    9.12

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -7,4 +7,16 @@ router.get('/', (_req, res) => {
   res.send(patientService.getNonSensitivePatientEntires());
 });
 
+router.post('/', (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const newPatientEntry = patientService.addEntry(
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation
+  );
+  res.json(newPatientEntry);
+})
+
 export default router;
--- part9/patientor-backend/src/services/patientService.ts
+++ part9/patientor-backend/src/services/patientService.ts
@@ -2,12 +2,12 @@ import patients from '../../data/patients';
 
 import { Patient, NonSensitivePatient } from '../types';
 
-const getEntries = (): Array<Patient> => {
+const getEntries = (): Patient[] => {
   return patients;
 };
 
-const getNonSensitivePatientEntires = (): NonSensitivePatient [] => {
-  return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({
+const getNonSensitivePatientEntires = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
     id,
     name,
     dateOfBirth,
@@ -16,8 +16,32 @@ const getNonSensitivePatientEntires = (): NonSensitivePatient [] => {
   }));
 };
 
-const addEntry = (): null => {
-  return null;
+
+//Generating guids, https://stackoverflow.com/a/26502275
+class Guid {
+  static newGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      const r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+}
+
+const addEntry = (
+  name: string, dateOfBirth: string, ssn: string, gender: string, occupation: string
+): Patient => {
+  const newPatientEntry = {
+    id: Guid.newGuid(),
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation
+  };
+
+  patients.push(newPatientEntry);
+  return newPatientEntry;
 };
 
 export default {
